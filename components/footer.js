628191fb9ac7b9175b65270609e51f1541f2b880
Tanishka <anjanasharma8448766174@gmail.com>
2024-08-02 08:28:33 +0530

    done

--- components/footer.js
+++ components/footer.js
@@ -1,16 +1,16 @@
 export const footer = () => {
     return (
         `<div id="footer">
-            <p class="text-center">Made with ❤️ by Mohit Sehrawat</p>
+            <p class="text-center">Made by ✨ Tanishka</p>
             <div>
                 <button class="btn btn-primary">
-                    <a href="https://www.linkedin.com/in/m-sehrawat/" target="_blank">LinkedIn</a>
+                    <a href="www.linkedin.com/in/tanishka-304953274" target="_blank">LinkedIn</a>
                 </button>
                 <button class="btn btn-success">
                     <a href="https://github.com/m-sehrawat" target="_blank">GitHub</a>
                 </button>
                 <button class="btn btn-danger">
-                    <a href="https://mohit-portfolio.vercel.app/" target="_blank">Portfolio</a>
+                    <a href="https://github.com/tanishkasharmaaa" target="_blank">Portfolio</a>
                 </button>
             </div>
         </div>`
